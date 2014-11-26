0041d0921e603685cee440776204a601b1186532
David Stancu <dstancu@nyu/edu>
2014-11-26 14:25:06 -0500

    updated build with 3x3 point render

--- Assets/Scenes/3x3/Scripts/GameController.cs
+++ Assets/Scenes/3x3/Scripts/GameController.cs
@@ -4,15 +4,94 @@ using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
-	private List<List<GameObject>> board;
+	public GameObject[,] board = new GameObject[3,3];
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
+		// Find wooden board to get bounds
+		var wood = GameObject.Find("Board");
+
 		// Draw points and lines
-//		for (int i=0; i < 9; ++i) {
-//			var point = GameObject.CreatePrimitive(PrimitiveType.Quad);
-//			point
-//		}
+		for (int i=0; i < 3; ++i) 
+        {
+			var point = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            point.name = "0, " + i.ToString();
+			point.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+            switch (i) 
+            {
+                case (0):
+                    {
+                        point.transform.position = new Vector3(-2.23f, wood.renderer.bounds.max.y - 1f, 0f);
+                        break;
+                    }
+                case (1):
+                    {
+                        point.transform.position = new Vector3(0f, wood.renderer.bounds.max.y - 1f, 0f);
+                        break;
+                    }
+                case (2):
+                    {
+                        point.transform.position = new Vector3(2.23f, wood.renderer.bounds.max.y - 1f, 0f);
+                        break;
+                    }
+            }
+            board[0, i] = point;
+		}
+
+        for (int i = 0; i < 3; ++i) 
+        {
+			var point = GameObject.CreatePrimitive(PrimitiveType.Quad);
+			point.name = "1, " + i.ToString();
+			point.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            switch (i)
+            {
+                case (0):
+                    {
+                        point.transform.position = new Vector3(-2.23f, 0f, 0f);
+                        break;
+                    }
+                case (1):
+                    {
+                        point.transform.position = new Vector3(0f, 0f, 0f);
+                        break;
+                    }
+                case (2):
+                    {
+                        point.transform.position = new Vector3(2.23f, 0f, 0f);
+                        break;
+                    }
+            }
+            board[1, i] = point;
+		}
+
+        for (int i = 0; i < 3; ++i)
+        {
+            var point = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            point.name = "2, " + i.ToString();
+            point.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+            switch (i)
+            {
+                case (0):
+                    {
+                        point.transform.position = new Vector3(-2.23f, -wood.renderer.bounds.max.y + 1f, 0f);
+                        break;
+                    }
+                case (1):
+                    {
+                        point.transform.position = new Vector3(0f, -wood.renderer.bounds.max.y + 1f, 0f);
+                        break;
+                    }
+                case (2):
+                    {
+                        point.transform.position = new Vector3(2.23f, -wood.renderer.bounds.max.y + 1f, 0f);
+                        break;
+                    }
+            }
+            board[2, i] = point;
+        }
 	}
 	
 	// Update is called once per frame
