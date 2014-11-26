65e24f635d3a6d780546fe64196b6e52c2c8b75f
David Stancu <dstancu@nyu.edu>
2014-11-26 11:39:28 -0500

    initial commit

---
+++ Assets/Scenes/3x3/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+	private List<List<GameObject>> board;
+
+	// Use this for initialization
+	void Start () {
+		// Draw points and lines
+//		for (int i=0; i < 9; ++i) {
+//			var point = GameObject.CreatePrimitive(PrimitiveType.Quad);
+//			point
+//		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scenes/StartGUI/Scripts/LoadGUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadGUI : MonoBehaviour {
+
+	public float x, y;
+	public GUISkin DefSkin;
+
+	// Use this for initialization
+	void Start () {
+		x = Screen.width;
+		y = Screen.height;
+
+	}
+
+	void OnGUI() {
+		// Apply skin
+		GUI.skin = Resources.Load("GUI/DefSkin") as GUISkin;
+
+		// Draw main box
+		GUI.Box(new Rect((x/2) - 200, (y/2) - 250, 400, 400), "Fanorona!");
+		if (GUI.Button(new Rect((x/2) - 175, (y/2) - 125, 350, 50), "Play 3x3"))
+		{
+			Application.LoadLevel("3x3");
+		}
+		if (GUI.Button(new Rect((x/2) - 175, (y/2) - 50, 350, 50), "Play 5x5"))
+		{
+			//
+		}
+		if (GUI.Button(new Rect((x/2) - 175, (y/2) + 25, 350, 50), "Quit"))
+		{
+			Application.Quit();
+		}
+
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
